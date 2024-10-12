0f636e517ba40febbaf670d467adba222fcdea63
Cedeno718 <cedeno718@morrisville.edu>
2024-10-12 02:51:24 -0400

    Implemented TextMeshProUGUI to display quiz questions dynamically

---
+++ Assets/Quiz.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
