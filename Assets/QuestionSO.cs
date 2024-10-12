948dd2b36cb57ae8d0488bce2348a842a75b35dc
Cedeno718 <cedeno718@morrisville.edu>
2024-10-12 01:38:20 -0400

    Created ScriptableObject for storing quiz questions and answers.

---
+++ Assets/QuestionSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Quiz Questions", menuName = "New Question")]
+public class QuestionSO : ScriptableObject {
+    [TextArea(2,6)]
+    [SerializeField] string question = "Enter new question text here.";
+
+}
