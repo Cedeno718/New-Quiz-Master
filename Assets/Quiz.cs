2978400068c8d7d85aa5acf9066d7ad509c2a059
Cedeno718 <cedeno718@morrisville.edu>
2024-10-20 23:16:15 -0400

    Implemented answer feedback with sprite swapping.

--- Assets/Quiz.cs
+++ Assets/Quiz.cs
@@ -2,30 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
     void Start()
     {
 
         questionText.text = question.GetQuestion();
 
-        for(int i = 0; i < answerButtons.Length; i++)
+        for (int i = 0; i < answerButtons.Length; i++)
         {
 
         TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-        buttonText.text= question.GetAnswer(i);
+        buttonText.text = question.GetAnswer(i);
         }
 
 
     }
+    public void OnAnswerSelected(int index)
+     {   
+        Image buttonImage;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Wrong Answer, it's this one;\n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
     }
 }
